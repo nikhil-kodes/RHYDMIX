c875ed843bd73c305df127c6933aa277c3a2681d
nikhil-kodes <knikhilsingh01@gmail.com>
2025-01-13 17:20:58 +0530

    added locationget feature

--- functions/fetchData.js
+++ functions/fetchData.js
@@ -1,8 +1,9 @@
 const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
+  const locationgot = event.queryStringParameters;
   const API_KEY = process.env.API_KEY; // Access the environment variable
-  const API_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=greater noida&aqi=yes`;
+  const API_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${locationgot}&aqi=yes`;
 
   try {
     const response = await fetch(API_URL);
