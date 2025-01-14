9c0f85c358fc425e8f5756652d7501b0b5df96b2
nikhil-kodes <knikhilsingh01@gmail.com>
2025-01-14 07:48:27 +0530

    Updated exports.handler

--- functions/fetchData.js
+++ functions/fetchData.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
-  const locationgot = event.queryStringParameters;
+  const locationgot = event.queryStringParameters.location;
   const API_KEY = process.env.API_KEY; // Access the environment variable
   const API_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${locationgot}&aqi=yes`;
 
