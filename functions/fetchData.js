e5bac9ca3c44c509d4a2ded5df82a916b06938b6
nikhil-kodes <knikhilsingh01@gmail.com>
2025-01-14 11:42:21 +0530

    Update fetchData.js

--- functions/fetchData.js
+++ functions/fetchData.js
@@ -4,6 +4,7 @@ exports.handler = async (event, context) => {
   const locationgot = event.queryStringParameters.location;
   const API_KEY = process.env.API_KEY; // Access the environment variable
   const API_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${locationgot}&aqi=yes`;
+  console.log(locationgot);
 
   try {
     const response = await fetch(API_URL);
